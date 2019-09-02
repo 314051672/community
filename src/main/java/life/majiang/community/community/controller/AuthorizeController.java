effa460c00b7c7785b2ed0635ba34bf225343859
gaotian <314051672@qq.com>
2019-09-02 14:53:34 +0800

    extract git hub config from code to yml

--- src/main/java/life/majiang/community/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/community/controller/AuthorizeController.java
@@ -1,18 +1,25 @@
 package life.majiang.community.community.controller;
 
-import life.majiang.community.community.dto.AccesstokenDTO;
-import life.majiang.community.community.dto.GithubUser;
-import life.majiang.community.community.provider.GithubProvider;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+        import life.majiang.community.community.dto.AccesstokenDTO;
+        import life.majiang.community.community.dto.GithubUser;
+        import life.majiang.community.community.mapper.UserMapper;
+        import life.majiang.community.community.model.User;
+        import life.majiang.community.community.provider.GithubProvider;
+        import org.springframework.beans.factory.annotation.Autowired;
+        import org.springframework.beans.factory.annotation.Value;
+        import org.springframework.stereotype.Controller;
+        import org.springframework.web.bind.annotation.GetMapping;
+        import org.springframework.web.bind.annotation.RequestParam;
+
+        import javax.servlet.http.HttpServletRequest;
+        import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
+    @Autowired
+    private UserMapper userMapper;
 
     @Value(value = "${github.client.id}")
     private String clientId;
@@ -20,18 +27,32 @@ public class AuthorizeController {
     private String clientSecret;
     @Value(value = "${github.redirect.uri}")
     private String redirectUri;
+
+
     @GetMapping("/callback")
     public String callback(@RequestParam(name = "code") String code,
-                           @RequestParam(name = "state") String state){
+                           @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccesstokenDTO accesstokenDTO = new AccesstokenDTO();
         accesstokenDTO.setClient_id(clientId);
         accesstokenDTO.setClient_secret(clientSecret);
         accesstokenDTO.setCode(code);
         accesstokenDTO.setRedirect_uri(redirectUri);
         accesstokenDTO.setState(state);
-        String accessToken=githubProvider.getAccessToken(accesstokenDTO);
-        GithubUser user=githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        String accessToken = githubProvider.getAccessToken(accesstokenDTO);
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null) {
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user", githubUser);
+            return "redirect:/";
+        } else {
+            return "redirect:/";
+        }
     }
 }
